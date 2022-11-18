48c78af3f56a627c34f91000119220f2717c5ffc
Thiago Pereira <thiagopereiramail@gmail.com>
2022-11-18 19:51:59 -0300

    Display sprite on screen

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
 import './style.css';
-import Icon from './icon.png';
-import printMe from './print';
+import spritesheet from './spritesheet.png';
 
-
-const div = document.createElement('div');
 const canvas = document.createElement('canvas');
-const icon = new Image();
-icon.src = Icon;
+const ctx = canvas.getContext('2d');
+document.body.appendChild(canvas);
 
-const btn = document.createElement('button');
-btn.innerText = 'Click here!';
-btn.onclick = printMe;
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+const SPRITE_WIDTH = 575;
+const SPRITE_HEIGHT = 523;
+const playerImage = new Image();
+playerImage.src = spritesheet;
 
-div.appendChild(canvas);
-div.appendChild(icon);
-div.appendChild(btn);
+let sx=0, sy=0;
 
-document.body.appendChild(div);
+function animate() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.drawImage(playerImage, 0, 0); v1
+  // ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); V2
+  // ctx.drawImage(playerImage, sx, sy, sw, sh, dx, dy, dw, dh); V3
+
+  ctx.drawImage(playerImage, sx, sy, SPRITE_WIDTH, SPRITE_HEIGHT, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  requestAnimationFrame(animate);
+}
+
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Sprite Animation',
     }),
   ],
   module: {
